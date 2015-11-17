96a99e5371178f5a692056ca4e4bb2db4fe5e312
Greg Mason <gmason@msu.edu>
2015-11-17 12:37:15 -0500

    stop tab-completing users

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -49,17 +49,17 @@ zstyle ':completion::complete:*' use-cache 1
 zstyle ':completion::complete:*' cache-path ~/.oh-my-zsh/cache/
 
 # Don't complete uninteresting users
-zstyle ':completion:*:*:*:users' ignored-patterns \
-        adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
-        dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
-        hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
-        mailman mailnull mldonkey mysql nagios \
-        named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
-        operator pcap postfix postgres privoxy pulse pvm quagga radvd \
-        rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs
-
+#zstyle ':completion:*:*:*:users' ignored-patterns \
+#        adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
+#        dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
+#        hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
+#        mailman mailnull mldonkey mysql nagios \
+#        named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
+#        operator pcap postfix postgres privoxy pulse pvm quagga radvd \
+#        rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs
+#
 # ... unless we really want to.
-zstyle '*' single-ignored show
+#zstyle '*' single-ignored show
 
 if [ "x$COMPLETION_WAITING_DOTS" = "xtrue" ]; then
   expand-or-complete-with-dots() {
